678e77d0da13994f93f5fb48f21c8c8e6bd55bd6
githmith <vuminhthanh2305@gmail.com>
2017-04-26 21:35:26 -0400

    Refactor Message. Create ContentMessage

---
+++ src/dvroutingsimulator/ContentMessage.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dvroutingsimulator;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author thanhvu
+ */
+public class ContentMessage extends Message{
+    int timeToLive; // max number of remaining hops, decremented at each router
+    ArrayList<Address> path; // addresses of routers in the path
+    String msg;
+    
+    
+    public ContentMessage(String srcip, int srcport, String dstip, int dstport, int remainingHops, String mess){
+        this.type = MsgType.CONTENT;
+        this.srcIP = srcip;
+        this.srcPort = srcport;
+        this.dstIP = dstip;
+        this.dstPort = dstport;
+        this.msg = msg;
+        this.timeToLive = remainingHops;
+        this.path = new ArrayList<Address>();
+    }
+    /**
+     * Constructor that parses a string representation of a ContentMessage
+     * Message's format: 
+     * "type[delimiter]timeToLive[delimiter]srcIP[delimiter]srcPort[delimiter]
+     * dstIP[delimiter]dstPort[delimiter]msg[delimiter]ip-port ip-port ..."
+     */
+    public ContentMessage(String text){
+        String[] fields = text.split(this.delimiter);
+        if(MsgType.CONTENT == MsgType.valueOf(fields[0])){
+            this.type = MsgType.CONTENT;
+            this.timeToLive = Integer.parseInt(fields[1]);
+            this.srcIP = fields[2];
+            this.srcPort = Integer.parseInt(fields[3]);
+            this.dstIP = fields[4];
+            this.dstPort = Integer.parseInt(fields[5]);
+            this.msg = fields[6];
+            this.path = new ArrayList<Address>();
+            if(fields.length > 7){
+                for(String address: fields[7].split(" ")){
+                    String[] tmp = address.split("-");
+                    this.path.add(new Address(tmp[0],Integer.parseInt(tmp[1])));
+                }
+            }
+        } else {
+            System.out.println("ERROR: Not a ContentMessage");
+        }
+    }
+    
+    public void addRouter(String ip, int port){
+        path.add(new Address(ip,port));
+    }
+    
+    public String toString(){
+        String output = type +delimiter+ timeToLive 
+                +delimiter+ srcIP +delimiter+ srcPort 
+                +delimiter+ dstIP +delimiter+ dstPort 
+                +delimiter+ msg +delimiter;
+        for(Address ad: path){
+            output += ad.ip +"-"+ ad.port +" ";
+        }
+        return output;
+    }
+    
+    public void reduceTimeTolive(){
+        timeToLive--;
+    }
+    
+    public String getMessage(){ return msg;}
+    
+    public int getTimeToLive(){ return timeToLive;}
+    
+    public ArrayList<Address> getPath(){ return path;}
+}
--- src/dvroutingsimulator/Message.java
+++ src/dvroutingsimulator/Message.java
@@ -11,59 +11,17 @@ import java.util.ArrayList;
  *
  * @author thanhvu
  */
+enum MsgType { DV, WEIGHT, CONTENT }
+
 public class Message {
-    int timeToLive; // max number of remaining hops, decremented at each router
-    String srcIP;
-    String dstIP;
-    int srcPort;
-    int dstPort;
-    String msg;
-    ArrayList<Address> routers;
-    // NEED TYPES: DV, WEIGHT, CONTENT
     
-    public Message(String srcip, int srcport, String dstip, int dstport, int remainingHops, String mess){
-        srcIP = srcip;
-        srcPort = srcport;
-        dstIP = dstip;
-        dstPort = dstport;
-        timeToLive = remainingHops;
-        msg = mess;
-        routers = new ArrayList<Address>();
-    }
-    
-    public Message(String text){
-        // text format: "timeToLive srcIP dstIP srcPort dstPort msg ip-port ip-port ip-port"
-        String[] fields = text.split(" ");
-        timeToLive = Integer.parseInt(fields[0]);
-        srcIP = fields[1];
-        dstIP = fields[2];
-        srcPort = Integer.parseInt(fields[3]);
-        dstPort = Integer.parseInt(fields[4]);
-        msg = fields[5];
-        routers = new ArrayList<Address>();
-        for(int i=6; i<fields.length; i++){
-            String[] tmp = fields[i].split("-");
-            routers.add(new Address(tmp[0],Integer.parseInt(tmp[1])));
-        }
-    }
-    
-    public void addRouter(String ip, int port){
-        routers.add(new Address(ip,port));
-    }
-    
-    public String toString(){
-        String output = timeToLive +" "+ srcIP +" "+ dstIP 
-                +" "+ srcPort +" "+ dstPort +" "+ msg;
-        for(Address ad: routers){
-            output += " " + ad.ip + "-" + ad.port;
-        }
-        return output;
-    }
-    
-    public void reduceTimeTolive(){
-        timeToLive--;
-    }
-    
+    protected String srcIP;
+    protected String dstIP;
+    protected int srcPort;
+    protected int dstPort;
+    protected MsgType type;
+    protected final String delimiter = "<<>>";
+   
     public String getSrcIP(){ return srcIP;}
     
     public String getDstIP(){ return dstIP;}
@@ -71,11 +29,5 @@ public class Message {
     public int getSrcPort(){ return srcPort;}
     
     public int getDstPort(){ return dstPort;}
-    
-    public String getMessage(){ return msg;}
-    
-    public int getTimeToLive(){ return timeToLive;}
-    
-    public ArrayList<Address> getRouters(){ return routers;}
     
 }
