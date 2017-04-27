5efa7d6485513fdbbb4dd2f589bcd96d81b1c58c
githmith <vuminhthanh2305@gmail.com>
2017-04-26 21:51:07 -0400

    Clean up ContentMessage

--- src/dvroutingsimulator/ContentMessage.java
+++ src/dvroutingsimulator/ContentMessage.java
@@ -29,29 +29,39 @@ public class ContentMessage extends Message{
     }
     /**
      * Constructor that parses a string representation of a ContentMessage
-     * Message's format: 
+     * @param text The message to parse. 
+     * Should have at least 7 fields separated by [delimiter] as followed:
+     * 
      * "type[delimiter]timeToLive[delimiter]srcIP[delimiter]srcPort[delimiter]
      * dstIP[delimiter]dstPort[delimiter]msg[delimiter]ip-port ip-port ..."
+     * 
      */
     public ContentMessage(String text){
         String[] fields = text.split(this.delimiter);
-        if(MsgType.CONTENT == MsgType.valueOf(fields[0])){
-            this.type = MsgType.CONTENT;
-            this.timeToLive = Integer.parseInt(fields[1]);
-            this.srcIP = fields[2];
-            this.srcPort = Integer.parseInt(fields[3]);
-            this.dstIP = fields[4];
-            this.dstPort = Integer.parseInt(fields[5]);
-            this.msg = fields[6];
-            this.path = new ArrayList<Address>();
-            if(fields.length > 7){
-                for(String address: fields[7].split(" ")){
-                    String[] tmp = address.split("-");
-                    this.path.add(new Address(tmp[0],Integer.parseInt(tmp[1])));
-                }
+        
+        if(fields.length < 7){
+            System.out.println("ERROR: Wrong format. Not enough fields.");
+            return;
+        }
+        
+        if(MsgType.CONTENT != MsgType.valueOf(fields[0])){
+            System.out.println("ERROR: Wrong type. Not a ContentMessage.");
+            return;
+        }
+        
+        this.type = MsgType.CONTENT;
+        this.timeToLive = Integer.parseInt(fields[1]);
+        this.srcIP = fields[2];
+        this.srcPort = Integer.parseInt(fields[3]);
+        this.dstIP = fields[4];
+        this.dstPort = Integer.parseInt(fields[5]);
+        this.msg = fields[6];
+        this.path = new ArrayList<Address>();
+        if (fields.length > 7) {
+            for (String address : fields[7].split(" ")) {
+                String[] tmp = address.split("-");
+                this.path.add(new Address(tmp[0], Integer.parseInt(tmp[1])));
             }
-        } else {
-            System.out.println("ERROR: Not a ContentMessage");
         }
     }
     
